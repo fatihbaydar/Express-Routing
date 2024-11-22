095899155afe44a446bf33a62cc0a74dc0a5c3ad
fatihbaydar <fatihbaydar2004@gmail.com>
2024-11-22 11:20:13 +0300

    http methods and urls

--- app.js
+++ app.js
@@ -1,4 +1,46 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESSJS
 ------------------------------------------------------- */
+
+//? HTTP METHODS & URLs:
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const HOST = process.env.HOST || "127.0.0.1";
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  // res.send("Welcome to express")
+  res.send({
+    message: "Welcome to express",
+  });
+});
+
+
+app.post("/", (req, res) => {
+  res.send({
+    message: "post",
+  });
+});
+
+app.put("/", (req, res) => {
+    res.send({
+      message: "put",
+    });
+  });
+
+app.delete("/", (req, res) => {
+    res.send({
+      message: "delete",
+    });
+  });
+
+
+
+// app.listen(PORT, () => {console.log(`running: http://127.0.0.1:8000`)})
+app.listen(PORT, () => {
+  console.log(`Running: http://${HOST}:${PORT}`);
+});
